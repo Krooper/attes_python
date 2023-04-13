d3ce199a86801e427025fcc4aca909c03488ce37
Krooper <98321262+Krooper@users.noreply.github.com>
2023-04-13 20:56:30 +0300

    moved file funcs to a separate py file

--- controllers/Notes_control.py
+++ controllers/Notes_control.py
@@ -1,7 +1,5 @@
 import json
-
 from datetime import datetime
-
 from models.Note import Note
 
 
--- main.py
+++ main.py
@@ -1,35 +1,5 @@
+from file_manager import create_file, get_file
 from views.Notes_view import Notes_view
-import os
-
-
-def get_file():
-    if not os.path.exists("notes.json"):
-        print("The file does not exists!\nCreating empty file...")
-        open("notes.json", "x")
-        return "notes.json"
-    else:
-        return "notes.json"
-
-
-def create_file():
-    file_name = "notes.json"
-    if not os.path.exists(file_name):
-        open(file_name, "x")
-    else:
-        print("The file exists!\nAre you sure you want to remove it? (input y or n)")
-        action = input()
-        if action == 'y' or action == "Y":
-            os.remove(file_name)
-            open(file_name, "x")
-        elif action == 'n' or action == "N":
-            print("Input a new name for a new file:")
-            file_name = input()
-            open(file_name, "x")
-        else:
-            print("No such command! Try again!")
-            return create_file()
-    file = file_name
-    return file
 
 
 def start():
